eb1fd2a37accab81b2d1c0f18d13a0ae26262028
Joonas Rantala <joonasrantala@hotmail.com>
2025-01-04 15:52:35 +0200

    added dist folder to launch online

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ morgan.token("body", (req, res) => {
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
 
 app.get("/api/persons", (request, response) => {
   response.json(persons)
